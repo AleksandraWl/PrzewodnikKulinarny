67293ab0d533f3244ebc6bc4ee3e9353c6629d89
MalaMi <aleksandra.e.wlodek@gmail.com>
2018-09-17 20:38:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/malami/przewodnikkulinarny/WyborJedzenia.java
@@ -0,0 +1,60 @@
+package com.example.malami.przewodnikkulinarny;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class WyborJedzenia extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wybor_jedzenia);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.wyloguj:
+                firebaseAuth.signOut();
+                Toast.makeText(this, "Wylogowano", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(WyborJedzenia.this, Logowanie.class);
+                startActivity(i);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void zatwierdz(View view) {
+
+    }
+}
+
